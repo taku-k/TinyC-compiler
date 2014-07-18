213b325ac7fc347fad6dfe22716674a3eec30cf6
KUWAHARA TAKUYA <a0124973@ws11.(none)>
2014-07-18 14:32:50 +0900

    デモ終了

--- tcc/test/c_fun.c
+++ tcc/test/c_fun.c
@@ -2,14 +2,7 @@
 
 chk(int x, int y)
 {
-  if (x == y) {
-    printf("OK\n");
-  }
-  else {
-    printf("NG\n");
-    printf("Return:   %d\n", x);
-    printf("Expected: %d\n", y);
-  }
+    printf(x == y ? "OK\n" : "NG\n");
 }
 
 /* for ss.c */
@@ -18,10 +11,10 @@ int vv[10] = {3, 5, 1, 8, 7, 6, 2, 10, 4, 9};
 
 int v(int i)
 {
-  return vv[i];
+    return vv[i];
 }
 
 set_v(i, x)
 {
-  vv[i] = x;
+    vv[i] = x;
 }
